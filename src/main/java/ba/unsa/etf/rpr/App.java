bbde69f7f000e7ba751b858c002886d565a17288
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-17 20:26:06 +0200

    Connected successfully to database with env

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,13 +1,24 @@
 package ba.unsa.etf.rpr;
 
-/**
- * Hello world!
- *
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Projekat";
+        String username = "freedb_edzanko1";
+        String password = System.getenv("MYSQL_PASS");
+
+        try {
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("Connection successful!");
+        } catch (SQLException e) {
+            System.out.println("Connection failed: " + e.getMessage());
+        }
     }
+
 }
