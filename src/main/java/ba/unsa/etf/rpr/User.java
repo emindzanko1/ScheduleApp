1a94fc53ce65f227f4502bfb5d4d5cdc9c3c6b3b
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-17 21:28:02 +0200

    Added two constructors in class User

--- src/main/java/ba/unsa/etf/rpr/User.java
+++ src/main/java/ba/unsa/etf/rpr/User.java
@@ -3,4 +3,18 @@ package ba.unsa.etf.rpr;
 public class User {
     private int id;
     private String username, password, salt, firstName, lastName, email;
+
+    public User(int id, String username, String password, String salt, String firstName, String lastName, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.salt = salt;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+    public User() {
+    }
 }
