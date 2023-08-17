0bbba876d86dfff0356e030df87edb502feaa736
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-17 22:41:50 +0200

    Added dummy logic preparation for CRUD

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -1,16 +1,58 @@
 package ba.unsa.etf.rpr;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class App 
 {
+    private static UserDAO userDAO;
     public static void main( String[] args ) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
 
-        UserDAO userDAO = UserDAO.getInstance();
+        userDAO = UserDAO.getInstance();
 
-       userDAO.pretraga();
+        int opcija = 0;
 
+        do {
+            System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n0 - kraj programa");
+            Scanner ulaz = new Scanner(System.in);
+            opcija = ulaz.nextInt();
+            switch (opcija) {
+                case 1:
+                    pretraga();
+                    break;
+                case 2:
+                    unos();
+                    break;
+                case 3:
+                    izmjena();
+                    break;
+                case 4:
+                    brisanje();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Nepoznata opcija");
+            }
+        }
+        while (opcija != 0);
+
+
+
+        userDAO.pretraga();
+
+    }
+
+    private static void unos() {
+    }
+
+    private static void brisanje() {
+    }
+
+    private static void izmjena() {
+    }
+
+    private static void pretraga() {
     }
 
 }
