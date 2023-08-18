1cc6ac2efa560477cd40ea117f94f4dc4f7d86c4
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-18 12:41:26 +0200

    Getting schedule name is working with main

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -15,40 +15,90 @@ public class App
 
         ulaz = new Scanner(System.in);
 
-        System.out.println("S kojom tabelom želite da radite?");
-
-
+        System.out.println("S kojom tabelom želite da radite? 1, 2 ili 3");
+        int brojTabele = ulaz.nextInt();
 
         int opcija = 0;
-
-        do {
-            System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n0 - kraj programa");
-            opcija = ulaz.nextInt();
-            if(ulaz.hasNextLine()) ulaz.nextLine();
-            switch (opcija) {
-                case 1:
-                    pretragaKorisnika();
-                    break;
-                case 2:
-                    unosKorisnika();
-                    break;
-                case 3:
-                    izmjenaKorisnika();
-                    break;
-                case 4:
-                    brisanjeKorisnika();
-                    break;
-                case 5:
-                    pretragaSvihKorisnika();
-                    break;
-                case 0:
-                    break;
-                default:
-                    System.out.println("Nepoznata opcija");
+        if(brojTabele == 1) {
+            do {
+                System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n0 - kraj programa");
+                opcija = ulaz.nextInt();
+                if(ulaz.hasNextLine()) ulaz.nextLine();
+                switch (opcija) {
+                    case 1:
+                        pretragaKorisnika();
+                        break;
+                    case 2:
+                        unosKorisnika();
+                        break;
+                    case 3:
+                        izmjenaKorisnika();
+                        break;
+                    case 4:
+                        brisanjeKorisnika();
+                        break;
+                    case 5:
+                        pretragaSvihKorisnika();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Nepoznata opcija");
+                }
             }
+            while (opcija != 0);
         }
-        while (opcija != 0);
+        else if(brojTabele == 2) {
+            do {
+                System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n0 - kraj programa");
+                opcija = ulaz.nextInt();
+                if(ulaz.hasNextLine()) ulaz.nextLine();
+                switch (opcija) {
+                    case 1:
+                        pretragaRasporeda();
+                        break;
+                    case 2:
+                        unosRasporeda();
+                        break;
+                    case 3:
+                        izmjenaRasporeda();
+                        break;
+                    case 4:
+                        brisanjeRasporeda();
+                        break;
+                    case 5:
+                        pretragaSvihRasporeda();
+                        break;
+                    case 0:
+                        break;
+                    default:
+                        System.out.println("Nepoznata opcija");
+                }
+            }
+            while (opcija != 0);
+        }
 
+
+    }
+
+    private static void unosRasporeda() {
+    }
+
+    private static void pretragaSvihRasporeda() {
+    }
+
+    private static void brisanjeRasporeda() {
+    }
+
+    private static void izmjenaRasporeda() {
+    }
+
+    private static void pretragaRasporeda() {
+        System.out.println("Unesite id rasporeda kojeg želite pretražiti: ");
+        int id = ulaz.nextInt();
+
+        for(Schedule schedule : scheduleDao.get(id))
+            System.out.println("Naziv rasporeda je: " + schedule.getScheduleName() + ".");
     }
 
     private static void unosKorisnika() {
