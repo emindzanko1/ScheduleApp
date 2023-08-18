cda50c09df514639a6ab46eba1c4af8911879195
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-18 11:26:51 +0200

    Added two constructors for Schedule class

--- src/main/java/ba/unsa/etf/rpr/Schedule.java
+++ src/main/java/ba/unsa/etf/rpr/Schedule.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr;
 
 public class Schedule {
+    private int id, userId;
+    private String scheduleName;
+
+    public Schedule(int id, int userId, String scheduleName) {
+        this.id = id;
+        this.userId = userId;
+        this.scheduleName = scheduleName;
+    }
+
+    public Schedule() {
+    }
 }
