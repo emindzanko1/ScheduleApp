ba711fee35e55f55722e9f9799dc704f2674325c
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-18 12:14:15 +0200

    Added private constructor in ScheduleSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/ScheduleSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/ScheduleSQLImplementation.java
@@ -1,9 +1,25 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScheduleSQLImplementation implements ScheduleDao {
+
+    private static ScheduleSQLImplementation instance = null;
+
+    private Connection conn;
+    private PreparedStatement pretragaUpit, dodavanjeUpit, noviIdUpit, izmjenaUpit, brisanjeUpit, sviUpit;
+
+    private ScheduleSQLImplementation() throws SQLException {
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Projekat";
+        String username = "freedb_edzanko1";
+        String password = System.getenv("MYSQL_PASS");
+        conn = DriverManager.getConnection(url, username, password);
+    }
 
     @Override
     public ArrayList<Schedule> get(int id) {
