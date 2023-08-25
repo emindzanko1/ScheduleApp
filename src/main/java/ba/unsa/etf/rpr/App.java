43150632ca3223778353ade9dab618b31671b982
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-25 22:23:50 +0200

    Main prepared for third table

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -15,74 +15,127 @@ public class App
 
         ulaz = new Scanner(System.in);
 
-        System.out.println("S kojom tabelom želite da radite? 1, 2 ili 3");
-        int brojTabele = ulaz.nextInt();
+        int opcija = 0, brojTabele = 0;
+        do {
+            System.out.println("S kojom tabelom želite da radite: 1, 2 ili 3? Unesite 0 ako ne želite da radite.");
+            brojTabele = ulaz.nextInt();
 
-        int opcija = 0;
-        if(brojTabele == 1) {
-            do {
-                System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n6 - pretraga po imenu\n0 - kraj programa");
-                opcija = ulaz.nextInt();
-                if(ulaz.hasNextLine()) ulaz.nextLine();
-                switch (opcija) {
-                    case 1:
-                        pretragaKorisnika();
-                        break;
-                    case 2:
-                        unosKorisnika();
-                        break;
-                    case 3:
-                        izmjenaKorisnika();
-                        break;
-                    case 4:
-                        brisanjeKorisnika();
-                        break;
-                    case 5:
-                        pretragaSvihKorisnika();
-                        break;
-                    case 6:
-                        pretragaKorisnikaPoImenu();
-                        break;
-                    case 0:
-                        break;
-                    default:
-                        System.out.println("Nepoznata opcija");
+            if (brojTabele == 1) {
+                do {
+                    System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n6 - pretraga po imenu\n0 - kraj programa");
+                    opcija = ulaz.nextInt();
+                    if (ulaz.hasNextLine()) ulaz.nextLine();
+                    switch (opcija) {
+                        case 1:
+                            pretragaKorisnika();
+                            break;
+                        case 2:
+                            unosKorisnika();
+                            break;
+                        case 3:
+                            izmjenaKorisnika();
+                            break;
+                        case 4:
+                            brisanjeKorisnika();
+                            break;
+                        case 5:
+                            pretragaSvihKorisnika();
+                            break;
+                        case 6:
+                            pretragaKorisnikaPoImenu();
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Nepoznata opcija");
+                    }
                 }
-            }
-            while (opcija != 0);
-        }
-        else if(brojTabele == 2) {
-            do {
-                System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n6 - pretraga po imenu\n0 - kraj programa");
-                opcija = ulaz.nextInt();
-                if(ulaz.hasNextLine()) ulaz.nextLine();
-                switch (opcija) {
-                    case 1:
-                        pretragaRasporeda();
-                        break;
-                    case 2:
-                        unosRasporeda();
-                        break;
-                    case 3:
-                        izmjenaRasporeda();
-                        break;
-                    case 4:
-                        brisanjeRasporeda();
-                        break;
-                    case 5:
-                        pretragaSvihRasporeda();
-                        break;
-                    case 6:
-                        pretragaRasporedaPoImenu();
-                        break;
-                    case 0:
-                        break;
-                    default:
-                        System.out.println("Nepoznata opcija");
+                while (opcija != 0);
+            } else if (brojTabele == 2) {
+                do {
+                    System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n6 - pretraga po imenu\n0 - kraj programa");
+                    opcija = ulaz.nextInt();
+                    if (ulaz.hasNextLine()) ulaz.nextLine();
+                    switch (opcija) {
+                        case 1:
+                            pretragaRasporeda();
+                            break;
+                        case 2:
+                            unosRasporeda();
+                            break;
+                        case 3:
+                            izmjenaRasporeda();
+                            break;
+                        case 4:
+                            brisanjeRasporeda();
+                            break;
+                        case 5:
+                            pretragaSvihRasporeda();
+                            break;
+                        case 6:
+                            pretragaRasporedaPoImenu();
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Nepoznata opcija");
+                    }
                 }
+                while (opcija != 0);
+            } else if (brojTabele == 3) {
+                do {
+                    System.out.println("Unesite opciju:\n1 - pretraga\n2 - unos\n3 - izmjena\n4 - brisanje\n5 - pretraga svih\n6 - pretraga po imenu\n0 - kraj programa");
+                    opcija = ulaz.nextInt();
+                    if (ulaz.hasNextLine()) ulaz.nextLine();
+                    switch (opcija) {
+                        case 1:
+                            pretragaSadržajaRasporeda();
+                            break;
+                        case 2:
+                            unosSadržajaRasporeda();
+                            break;
+                        case 3:
+                            izmjenaSadržajaRasporeda();
+                            break;
+                        case 4:
+                            brisanjeSadržajaRasporeda();
+                            break;
+                        case 5:
+                            pretragaSvihSadržajaRasporeda();
+                            break;
+                        case 6:
+                            pretragaSadržajaRasporedaPoImenu();
+                            break;
+                        case 0:
+                            break;
+                        default:
+                            System.out.println("Nepoznata opcija");
+                    }
+                }
+                while (opcija != 0);
             }
-            while (opcija != 0);
-        }
+            else {
+                System.out.println("Vidim da ne želite raditi, doviđenja.");
+            }
+        } while(brojTabele != 0);
+    }
+
+    private static void pretragaSadržajaRasporedaPoImenu() {
+    }
+
+    private static void pretragaSvihSadržajaRasporeda() {
+    }
+
+    private static void brisanjeSadržajaRasporeda() {
+    }
+
+    private static void izmjenaSadržajaRasporeda() {
+    }
+
+    private static void unosSadržajaRasporeda() {
+    }
+
+    private static void pretragaSadržajaRasporeda() {
     }
 
     private static User unosKorisnikaDuplication(int id) {
