e3efe625cf62c822f707ff5e5ef435b175b5a5af
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-25 22:15:52 +0200

    Added constructor, getInstance and removeInstance in ScheduleItemSQLImplementation class

--- src/main/java/ba/unsa/etf/rpr/ScheduleItemSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/ScheduleItemSQLImplementation.java
@@ -1,9 +1,35 @@
 package ba.unsa.etf.rpr;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ScheduleItemSQLImplementation implements ScheduleItemDao{
+    private static ScheduleItemSQLImplementation instance = null;
+
+    private Connection conn;
+    private PreparedStatement pretragaUpit, dodavanjeUpit, noviIdUpit, izmjenaUpit, brisanjeUpit, sviUpit, poImenuUpit;
+
+    private ScheduleItemSQLImplementation() throws SQLException {
+        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Projekat";
+        String username = "freedb_edzanko1";
+        String password = System.getenv("MYSQL_PASS");
+        conn = DriverManager.getConnection(url, username, password);
+    }
+
+    public static ScheduleItemSQLImplementation getInstance() throws SQLException {
+        if (instance == null) instance = new ScheduleItemSQLImplementation();
+        return instance;
+    }
+
+    public static void removeInstance() throws SQLException {
+        if (instance == null) return;
+        instance.conn.close();
+        instance = null;
+    }
     @Override
     public ArrayList<ScheduleItem> get(int id) {
         return null;
