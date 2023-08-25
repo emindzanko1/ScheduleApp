68fd00d1ab2cf44515484f178dc1534244e08094
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-25 22:09:25 +0200

    Added ScheduleItemSQLImplementation

---
+++ src/main/java/ba/unsa/etf/rpr/ScheduleItemSQLImplementation.java
@@ -0,0 +1,36 @@
+package ba.unsa.etf.rpr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScheduleItemSQLImplementation implements ScheduleItemDao{
+    @Override
+    public ArrayList<ScheduleItem> get(int id) {
+        return null;
+    }
+
+    @Override
+    public List<ScheduleItem> getAll() {
+        return null;
+    }
+
+    @Override
+    public void save(ScheduleItem scheduleItem) {
+
+    }
+
+    @Override
+    public void update(ScheduleItem scheduleItem) {
+
+    }
+
+    @Override
+    public void delete(ScheduleItem scheduleItem) {
+
+    }
+
+    @Override
+    public List<ScheduleItem> getByEventName(String eventName) {
+        return null;
+    }
+}
