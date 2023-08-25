0df78d67857dfb1fc71f6bc78dcf5c03e03c4678
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-25 22:11:59 +0200

    Added constructors, getters and setters in ScheduleItem class

--- src/main/java/ba/unsa/etf/rpr/ScheduleItem.java
+++ src/main/java/ba/unsa/etf/rpr/ScheduleItem.java
@@ -1,4 +1,72 @@
 package ba.unsa.etf.rpr;
 
 public class ScheduleItem {
+    private int id, scheduleId;
+    private String dayOfWeek, startTime, endTime, eventName, location;
+
+    public ScheduleItem(int id, int scheduleId, String dayOfWeek, String startTime, String endTime, String eventName, String location) {
+        this.id = id;
+        this.scheduleId = scheduleId;
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.eventName = eventName;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(int scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String getDayOfWeek() {
+        return dayOfWeek;
+    }
+
+    public void setDayOfWeek(String dayOfWeek) {
+        this.dayOfWeek = dayOfWeek;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
