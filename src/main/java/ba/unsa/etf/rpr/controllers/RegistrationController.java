ae6e1c39b3f68b986356fe9d524955b848c7271f
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-28 16:38:12 +0200

    Added switchToLogin method

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -1,9 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class RegistrationController {
 
@@ -44,4 +52,9 @@ public class RegistrationController {
                invalidPasswordId.setText("Password must have at least 5 characters");
        });
    }
+
+    public void switchToLogin(ActionEvent actionEvent) throws IOException {
+       Stage stage = (Stage) usernameId.getScene().getWindow();
+       stage.close();
+    }
 }
