8f6882eb77ac469630ac0ecbdb3a9aca8a09fb92
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-28 15:56:37 +0200

    Added labels that will indicate wrong inputs

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -9,9 +9,15 @@ public class RegistrationController {
 
     public TextField nameId;
 
+    public Label invalidName;
+
     public TextField usernameId;
 
+    public Label invalidUsername;
+
     public PasswordField passwordId;
+
+    public Label invalidPassword;
 
     private String username, password;
    public RegistrationController(String username, String password) {
