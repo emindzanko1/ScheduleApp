6005214974b8863feb0cabc8f703283c195b3e45
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-28 16:18:14 +0200

    Added username frontend validation

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -9,15 +9,15 @@ public class RegistrationController {
 
     public TextField nameId;
 
-    public Label invalidName;
+    public Label invalidNameId;
 
     public TextField usernameId;
 
-    public Label invalidUsername;
+    public Label invalidUsernameId;
 
     public PasswordField passwordId;
 
-    public Label invalidPassword;
+    public Label invalidPasswordId;
 
     private String username, password;
    public RegistrationController(String username, String password) {
@@ -29,5 +29,20 @@ public class RegistrationController {
     public void initialize() {
        usernameId.setText(username);
        passwordId.setText(password);
+
+       usernameId.textProperty().addListener((obs, oldValue, newValue) -> {
+           if(newValue.length() >= 5)
+               invalidUsernameId.setText(" ");
+           else
+               invalidUsernameId.setText("Username must have at least 5 characters");
+       });
+
+       passwordId.textProperty().addListener((obs, oldValue, newValue) -> {
+           if(newValue.length() >= 5)
+               passwordId.setText(" ");
+           else
+               passwordId.setText("Password must have at least 5 characters");
+       });
+
    }
 }
