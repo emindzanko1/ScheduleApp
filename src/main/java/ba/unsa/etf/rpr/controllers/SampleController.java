52cfbbdc7a206fdacb6571edec4a3583a01c9f4f
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-28 13:39:56 +0200

    Added ids for fields and buttons

--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -1,8 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class SampleController {
+    public Button cancelButtonId;
+    public Button okButtonId;
+    public TextField usernameId;
+    public PasswordField passwordId;
+
     public void probaDugmeta(ActionEvent actionEvent) {
         System.out.println("uspjesna proba");
     }
