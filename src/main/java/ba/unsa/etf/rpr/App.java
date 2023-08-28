97a00f9e6a7077cc0cffa20504285e13cdbf37c7
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-28 12:55:05 +0200

    The stage is not resizable, and looks way nicer

--- src/main/java/ba/unsa/etf/rpr/App.java
+++ src/main/java/ba/unsa/etf/rpr/App.java
@@ -15,6 +15,7 @@ public class App extends Application {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("sample.fxml"));
         primaryStage.setTitle("Hello, World!");
         primaryStage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
     public static void main(String[] args) {
