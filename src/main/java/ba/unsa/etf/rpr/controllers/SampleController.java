ca31eaba8fb62d4c8132d4ea92a31bf51fb1c1c5
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-28 13:34:45 +0200

    Added SampleController class and connected successfully with  sample.fxml

---
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -0,0 +1,9 @@
+package ba.unsa.etf.rpr.controllers;
+
+import javafx.event.ActionEvent;
+
+public class SampleController {
+    public void probaDugmeta(ActionEvent actionEvent) {
+        System.out.println("uspjesna proba");
+    }
+}
