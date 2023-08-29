a70f513dfbbf200f54111857b2a27b5d8e1986da
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-29 21:38:28 +0200

    User can't be logged in if it's not registered

--- src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/SampleController.java
@@ -1,17 +1,18 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.User;
+import ba.unsa.etf.rpr.UserSQLImplementation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -38,13 +39,31 @@ public class SampleController {
         stage.show();
     }
 
-    public void login(ActionEvent actionEvent) throws IOException {
-        Stage stage = new Stage();
-        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/schedule.fxml"));
-        ScheduleController controller = new ScheduleController();
-        loader.setController(controller);
-        stage.setTitle("ScheduleApp");
-        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
-        stage.show();
+    public void login(ActionEvent actionEvent) throws IOException, SQLException {
+        String username = usernameId.getText();
+        List<User> user = UserSQLImplementation.getInstance().getByUsername(username);
+
+        if(!user.isEmpty()) {
+            System.out.println("username " + username + " .");
+            Stage stage = new Stage();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/schedule.fxml"));
+            ScheduleController controller = new ScheduleController();
+            loader.setController(controller);
+            stage.setTitle("ScheduleApp");
+            stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+            stage.show();
+        }
+        else {
+            showAlert("Login Error", "Invalid username.");
+        }
+
+    }
+
+    private void showAlert(String title, String content) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(content);
+        alert.showAndWait();
     }
 }
