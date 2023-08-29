d7297c9ebcdd66e3151a3dc93ac0331a9c7030ce
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-29 23:54:41 +0200

    Property file used in UserSQLImplementation

--- src/main/java/ba/unsa/etf/rpr/UserSQLImplementation.java
+++ src/main/java/ba/unsa/etf/rpr/UserSQLImplementation.java
@@ -2,22 +2,31 @@ package ba.unsa.etf.rpr;
 
 import ba.unsa.etf.rpr.exceptions.ScheduleException;
 
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class UserSQLImplementation implements UserDao  {
 
     private static UserSQLImplementation instance = null;
 
-    private final Connection conn;
+    private Connection conn;
     private final PreparedStatement pretragaUpit, dodavanjeUpit, noviIdUpit, izmjenaUpit, brisanjeUpit, sviUpit, poImenuUpit;
 
     private UserSQLImplementation() throws SQLException {
-        String url = "jdbc:mysql://sql.freedb.tech:3306/freedb_RPR Projekat";
-        String username = "freedb_edzanko1";
-        String password = System.getenv("MYSQL_PASS");
-        conn = DriverManager.getConnection(url, username, password);
+        Properties p = new Properties();
+        try {
+            p.load(ClassLoader.getSystemResource("application.properties").openStream());
+            String url = p.getProperty("db.url");
+            String username = p.getProperty("db.username");
+            String password = p.getProperty("db.password");
+            conn = DriverManager.getConnection(url, username, password);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         pretragaUpit = conn.prepareStatement("SELECT * FROM User WHERE User_ID=?");
         noviIdUpit = conn.prepareStatement("SELECT MAX(User_ID)+1 FROM User");
         dodavanjeUpit = conn.prepareStatement("INSERT INTO User VALUES(?,?,?,?,?)");
