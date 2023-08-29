3f1794ecb182d14a807df9889683d9430525f504
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-29 23:17:29 +0200

    Updated Dao interface to throw ScheduleException

--- src/main/java/ba/unsa/etf/rpr/Dao.java
+++ src/main/java/ba/unsa/etf/rpr/Dao.java
@@ -1,18 +1,20 @@
 package ba.unsa.etf.rpr;
 
+import ba.unsa.etf.rpr.exceptions.ScheduleException;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public interface Dao<T> {
 
-    ArrayList<T> get(int id);
+    ArrayList<T> get(int id) throws ScheduleException;
 
-    List<T> getAll();
+    List<T> getAll() throws ScheduleException;
 
-    void save(T t);
+    void save(T t) throws ScheduleException;
 
-    void update(T t);
+    void update(T t) throws ScheduleException;
 
-    void delete(T t);
+    void delete(T t) throws ScheduleException;
 }
 
