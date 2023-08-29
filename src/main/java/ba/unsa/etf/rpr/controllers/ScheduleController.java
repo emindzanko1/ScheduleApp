5aecb11d3a4061b12276a72153240416c2d0c24a
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-29 21:22:04 +0200

    Added some logic to add button

--- src/main/java/ba/unsa/etf/rpr/controllers/ScheduleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ScheduleController.java
@@ -1,16 +1,24 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class ScheduleController {
 
+    @FXML
     public Button logoutId;
+    public Button addId;
 
     public void logout(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) logoutId.getScene().getWindow();
+        stage.close();
+    }
+    public void addSchedule(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) logoutId.getScene().getWindow();
         stage.close();
     }
