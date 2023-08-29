48a58e64e4c62aa27b1ad1b34dfc8cd0ccb77198
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-30 00:10:43 +0200

    ScheduleController cleanup code

--- src/main/java/ba/unsa/etf/rpr/controllers/ScheduleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ScheduleController.java
@@ -3,22 +3,18 @@ package ba.unsa.etf.rpr.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class ScheduleController {
 
     @FXML
     public Button logoutId;
-    public Button addId;
 
-    public void logout(ActionEvent actionEvent) throws IOException {
+    public void logout() {
         Stage stage = (Stage) logoutId.getScene().getWindow();
         stage.close();
     }
-    public void addSchedule(ActionEvent actionEvent) throws IOException {
+    public void addSchedule() {
         Stage stage = (Stage) logoutId.getScene().getWindow();
         stage.close();
     }
