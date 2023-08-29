67435eb8c8dd1f2992071addb52f0ec13f0fa003
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-29 19:48:31 +0200

    Added logout functionality

--- src/main/java/ba/unsa/etf/rpr/controllers/ScheduleController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ScheduleController.java
@@ -1,6 +1,19 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class ScheduleController {
+
+    public Button logoutId;
     public ScheduleController() {
+    }
+
+    public void logout(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) logoutId.getScene().getWindow();
+        stage.close();
     }
 }
