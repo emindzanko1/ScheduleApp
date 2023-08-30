871ca63dd3fd847777c796cad498513c0222bdb8
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-30 14:13:51 +0200

    Added functionality to cancel button

--- src/main/java/ba/unsa/etf/rpr/controllers/ScheduleFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ScheduleFormController.java
@@ -2,13 +2,17 @@ package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 public class ScheduleFormController {
 
     private String scheduleName;
     @FXML
     private Label title;
+    @FXML
+    private Button cancelId;
 
     public ScheduleFormController(String scheduleName) {
         this.scheduleName = scheduleName;
@@ -19,9 +23,11 @@ public class ScheduleFormController {
         title.setText(scheduleName);
     }
 
-    public void addSchedule(ActionEvent actionEvent) {
+    public void addScheduleItems(ActionEvent actionEvent) {
     }
 
-    public void logout(ActionEvent actionEvent) {
+    public void cancel(ActionEvent actionEvent) {
+        Stage stage = (Stage) cancelId.getScene().getWindow();
+        stage.close();
     }
 }
