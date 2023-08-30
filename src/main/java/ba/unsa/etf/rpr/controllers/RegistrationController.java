819d518a93b2b3056526b47589fb1cdf24536cf0
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-30 11:44:04 +0200

    User page is opened when register

--- src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/RegistrationController.java
@@ -90,8 +90,8 @@ public class RegistrationController {
             UserSQLImplementation userSQL = UserSQLImplementation.getInstance();
             userSQL.save(newUser);
             Stage stage = new Stage();
-            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/schedule.fxml"));
-            ScheduleController controller = new ScheduleController();
+            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/main.fxml"));
+            MainController controller = new MainController();
             loader.setController(controller);
             stage.setTitle("ScheduleApp");
             stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
