8e925a1d7fc47a56dce836157251c00a5c737437
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-30 14:56:13 +0200

    Schedule items connected to other components

--- src/main/java/ba/unsa/etf/rpr/controllers/ScheduleFormController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ScheduleFormController.java
@@ -1,10 +1,17 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.ScheduleSQLImplementation;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
 public class ScheduleFormController {
 
@@ -23,7 +30,14 @@ public class ScheduleFormController {
         title.setText(scheduleName);
     }
 
-    public void addScheduleItems(ActionEvent actionEvent) {
+    public void addScheduleItems(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("fxml/scheduleItems.fxml"));
+        ScheduleItemsController scheduleItemsController= new ScheduleItemsController();
+        loader.setController(scheduleItemsController);
+        stage.setTitle("ScheduleApp");
+        stage.setScene(new Scene(loader.load(), USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
+        stage.show();
     }
 
     public void cancel(ActionEvent actionEvent) {
