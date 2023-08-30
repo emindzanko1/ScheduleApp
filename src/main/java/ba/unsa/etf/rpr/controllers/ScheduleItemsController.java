31bb1d4c8c1c103c8a3f3bedb6f6d60d582d674d
emindzanko1 <edzanko1@etf.unsa.ba>
2023-08-30 15:12:27 +0200

    Added basic functionality to schedule items buttons

--- src/main/java/ba/unsa/etf/rpr/controllers/ScheduleItemsController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ScheduleItemsController.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
 public class ScheduleItemsController {
+
+    @FXML
+    public Button addButtonId;
+    public Button cancelButtonId;
+
+    @FXML
+    public void initalize() {
+
+    }
+
+    public void addScheduleItems() {
+        Stage stage = (Stage) cancelButtonId.getScene().getWindow();
+        stage.close();
+    }
+
+    public void cancelAddingScheduleItems() {
+        Stage stage = (Stage) cancelButtonId.getScene().getWindow();
+        stage.close();
+    }
 }
